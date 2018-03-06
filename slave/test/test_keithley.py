9842380c9f0cb38e3b5bd45cae9a6e007f79c5aa
t-onoz <18749921+t-onoz@users.noreply.github.com>
2018-03-06 14:55:01 +0900

    added is_command tests

--- slave/test/test_keithley.py
+++ slave/test/test_keithley.py
@@ -8,6 +8,7 @@ import collections
 import pytest
 from itertools import product
 
+from slave.driver import Command
 from slave.keithley import K2182, K6221, K2400
 from slave.transport import SimulatedTransport
 
@@ -25,10 +26,16 @@ def test_K6221():
 class TestK2400(object):
     ATOL = 1e-9
 
+    @staticmethod
+    def is_command(obj, attr):
+        return isinstance(object.__getattribute__(obj, attr), Command)
+
     def test_setup_triggering(self):
         k2400 = K2400(SimulatedTransport())
         for count, delay in product((1, 5, None), (1, 0.1, None)):
             k2400.setup.triggering(count, delay)
+            assert self.is_command(k2400.triggering, 'count')
+            assert self.is_command(k2400.triggering, 'delay')
             if count is not None:
                 assert k2400.triggering.count == count
             if delay is not None:
@@ -38,9 +45,12 @@ class TestK2400(object):
         k2400 = K2400(SimulatedTransport())
         for func, value in product(('voltage', 'current'), (1, -1, None)):
             k2400.setup.fixed_source(func, value)
+            subsource = getattr(k2400.source, func)
+            assert self.is_command(k2400.source, 'function_mode')
+            assert self.is_command(subsource, 'level')
+            assert self.is_command(subsource, 'level_triggered')
             assert k2400.source.function_mode == func
             if value is not None:
-                subsource = getattr(k2400.source, func)
                 assert abs(subsource.level - value) < self.ATOL
                 assert abs(subsource.level_triggered - value) < self.ATOL
 
@@ -48,6 +58,7 @@ class TestK2400(object):
         k2400 = K2400(SimulatedTransport())
         for enable, mode in product((True, False, None), ('auto', 'manual', None)):
             k2400.setup.resistance(enable, mode)
+            assert self.is_command(k2400.sense.resistance, 'mode')
             if enable is not None:
                 pass
                 # assert 'resistance' in k2400.sense.functions  # only works when connected to physical device
@@ -71,6 +82,11 @@ class TestK2400(object):
             else:
                 k2400.setup.sense(function, auto_range, range_, nplc, compliance, four_wire, integration_time)
                 subsense = getattr(k2400.sense, function)
+                assert self.is_command(subsense, 'auto_range')
+                assert self.is_command(subsense, 'range')
+                assert self.is_command(subsense, 'nplc')
+                assert self.is_command(subsense, 'compliance')
+                assert self.is_command(k2400.sense, 'four_wire')
                 if auto_range is not None:
                     assert subsense.auto_range == auto_range
                 if range_ is not None:
@@ -93,8 +109,12 @@ class TestK2400(object):
                 (10, 100)
         ):
             k2400.setup.sweep_source(function, start, stop, points)
-            assert k2400.source.function_mode == function
             subsource = getattr(k2400.source, function)
+            assert self.is_command(k2400.source, 'function_mode')
+            assert self.is_command(subsource, 'sweep_start')
+            assert self.is_command(subsource, 'sweep_stop')
+            assert self.is_command(subsource, 'sweep_points')
+            assert k2400.source.function_mode == function
             assert abs(subsource.sweep_start - start) < self.ATOL
             assert abs(subsource.sweep_stop - stop) < self.ATOL
             assert subsource.sweep_points == points
